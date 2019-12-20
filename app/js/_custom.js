0b5008f2791790061a65883249df6d4473324886
YakovSPb <yakov_dux@bk.ru>
2019-12-20 20:59:31 +0300

    Header,aside are ready

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	// Custom JS
-
+	// ASIDE MENU START
+	// READ MORE STAR
+$('.aside__link--has_sub').click(function(){
+if($(this).next().is(':hidden')){
+  $(this).next().slideDown('slow');
+} else {
+   $(this).next().slideUp('slow');
+}
+});
+	// ASIDE MENU END
+});
+$('.aside__link--has_sub').click(function(e){
+	e.preventDefault();
+});
+$('.aside__link--has_sub').click(function(){
+	$(this).parent().toggleClass('active-toggle');
 });
